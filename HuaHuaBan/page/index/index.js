040d4c253d7b7090fed6ad48c97ebd1f4dec96a7
Anran <anran@Anrans-MacBook-Pro.local>
2018-02-19 15:43:27 +0000

    repeal backup stroke
    Signed-off-by: Anran <anran@Anrans-MacBook-Pro.local>

--- HuaHuaBan/page/index/index.js
+++ HuaHuaBan/page/index/index.js
@@ -3,9 +3,9 @@
 // 获取全局应用程序实例对象
 const app = getApp();
 var ctx = null;
-var datax = new Array();
+var datax = new Array();//total data
 var datay = new Array();
-var linex = new Array();
+var linex = new Array();//line data
 var liney = new Array();
 // 创建页面实例对象
 Page({
@@ -20,24 +20,19 @@ Page({
   data: {
     
     showBgSet: false,
-    showPenSet:false,
-    //是否开启橡皮擦
-    isClear: false, 
-    
+    showPenSet:false, 
     defaultBgColor: '#e3e3e3',
     defaultPenColor: '#000000',
     
     //画布背景颜色数据
     canvasBgData:{
       canvasBgColor: "#e3e3e3",
-      colors: ['#000000', '#434343', '#666666', '#cccccc', '#e3e3e3', '#ffffff', '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9900ff']
     },
     //画笔数据
     
     penData: {
       penSize: 1,
       color: '#000000',
-      colors: ['#000000', '#434343', '#666666', '#cccccc', '#e3e3e3', '#ffffff', '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9900ff']
     },
   },
 
@@ -62,12 +57,11 @@ Page({
     bg.canvasBgColor = this.data.defaultBgColor;
     var pen = this.data.penData;
     pen.color = this.data.defaultPenColor;
-    pen.penSize = 5;
+    pen.penSize = 1;
 
     this.setData({
       showBgSet: false,
       showPenSet: false,
-      isClear: false, 
       canvasBgData: bg,
       penData: pen
     })
@@ -127,23 +121,12 @@ Page({
     });
     that.pointData.begin_x = e.touches[0].x;
     that.pointData.begin_y = e.touches[0].y;
-  
-    if (that.data.isClear) { //判断是否启用的橡皮擦功能  ture表示清除  false表示画画
-      ctx.setStrokeStyle(that.data.canvasBgData.canvasBgColor); //设置线条样式 此处设置为画布的背景颜色  橡皮擦原理就是：利用擦过的地方被填充为画布的背景颜色一致 从而达到橡皮擦的效果
-      ctx.setLineCap('round'); //设置线条端点的样式
-      ctx.setLineJoin('round'); //设置两线相交处的样式
-      ctx.setLineWidth(20); //设置线条宽度
-      ctx.save();  //保存当前坐标轴的缩放、旋转、平移信息
-      ctx.beginPath(); //开始一个路径
-      ctx.arc(that.pointData.begin_x, that.pointData.begin_y, 5, 0, 2 * Math.PI, true);  //添加一个弧形路径到当前路径，顺时针绘制  这里总共画了360度  也就是一个圆形
-      ctx.fill();  //对当前路径进行填充
-      ctx.restore();  //恢复之前保存过的坐标轴的缩放、旋转、平移信息
-    } else {
-      ctx.setStrokeStyle(that.data.penData.color);
-      ctx.setLineWidth(that.data.penData.penSize);
-      ctx.setLineCap('round'); // 让线条圆润
-      ctx.beginPath();
-    }
+
+    ctx.setStrokeStyle(that.data.penData.color);
+    ctx.setLineWidth(that.data.penData.penSize);
+    ctx.setLineCap('round'); // 让线条圆润
+    ctx.beginPath();
+
     ctx.moveTo(that.pointData.begin_x, that.pointData.begin_y);
     ctx.lineTo(that.pointData.begin_x, that.pointData.begin_y);
     linex.push(that.pointData.begin_x.toFixed(2));
@@ -153,20 +136,12 @@ Page({
   },
   move: function (e) {
     var that = this;
-   
-    if (that.data.isClear) { //判断是否启用的橡皮擦功能  ture表示清除  false表示画画
-      ctx.save();  //保存当前坐标轴的缩放、旋转、平移信息
-      ctx.moveTo(that.pointData.begin_x, that.pointData.begin_y);  //把路径移动到画布中的指定点，但不创建线条
-      ctx.lineTo(e.touches[0].x, e.touches[0].y);  //添加一个新点，然后在画布中创建从该点到最后指定点的线条
-      ctx.stroke();  //对当前路径进行描边
-      ctx.restore()  //恢复之前保存过的坐标轴的缩放、旋转、平移信息
-    } else {
-      ctx.moveTo(that.pointData.begin_x, that.pointData.begin_y);  //把路径移动到画布中的指定点，但不创建线条
-      ctx.lineTo(e.touches[0].x, e.touches[0].y);  //添加一个新点，然后在画布中创建从该点到最后指定点的线条
-      linex.push(e.touches[0].x.toFixed(2));
-      liney.push(e.touches[0].y.toFixed(2));
-      ctx.stroke();  //对当前路径进行描边
-    }
+    ctx.moveTo(that.pointData.begin_x, that.pointData.begin_y);  //把路径移动到画布中的指定点，但不创建线条
+    ctx.lineTo(e.touches[0].x, e.touches[0].y);  //添加一个新点，然后在画布中创建从该点到最后指定点的线条
+    linex.push(e.touches[0].x.toFixed(2));
+    liney.push(e.touches[0].y.toFixed(2));
+    ctx.stroke();  //对当前路径进行描边
+    
     ctx.draw(true); 
     that.pointData.begin_x = e.touches[0].x;
     that.pointData.begin_y = e.touches[0].y;
@@ -179,64 +154,37 @@ Page({
     console.log(datax);
     console.log(datay);
   },
-
-  //以下为自定义点击事件
-  openBgSet:function(){
-    this.setData({
-      showBgSet:true,
-      isClear: false
-    })
-  },
-  setBgColor:function (e) {
+  
+  eraseLaststroke: function (){
     var that = this;
-    wx.showModal({
-      title: '提示',
-      content: '设置背景将清空画布',
-      success: function (res) {
-        if (res.confirm) {
-          var color = e.target.dataset.color;
-          that.data.canvasBgData.canvasBgColor = color;
-          ctx.rect(0, 0, 1000, 2000);
-          ctx.setFillStyle(color);
-          ctx.fill();
-          ctx.draw();
-        } 
-        that.setData({
-          showBgSet: false
-        })
+    ctx.draw();//清空
+    that.resetColor();
+    //重绘
+   
+    ctx.setLineCap('round'); //设置线条端点的样式
+    ctx.setLineJoin('round'); //设置两线相交处的样式
+    ctx.save();  //保存当前坐标轴的缩放、旋转、平移信息
+    ctx.beginPath(); //开始一个路径
+    var stroke_num = datax.length;
+    if (stroke_num > 0){
+      
+     ctx.setLineWidth(that.data.penData.penSize);
+     for (var j = 0; j < stroke_num-1; j++)
+     {
+       var last_line_x = datax[j];
+       var last_line_y = datay[j];
+       for (var i = 0; i < last_line_x.length-1; i++){
+        ctx.moveTo(last_line_x[i], last_line_y[i]);
+        ctx.lineTo(last_line_x[i+1], last_line_y[i+1]);
+        ctx.stroke(); } //对当前路径进行描边    
       }
-    })
+     ctx.restore()  //恢复之前保存过的坐标轴的缩放、旋转、平移信息
+     ctx.draw(true);
+     datax.pop();
+     datay.pop();
+    }
   },
 
-
-  openPenSet: function () {
-    this.setData({
-      showPenSet: true,
-      isClear: false
-    })
-  },
-  
-  setPenSize:function(e){
-    var data = this.data.penData;
-    data.penSize = e.detail.value;
-    this.setData({
-      penData: data
-    })
-  },
-  setPenColor:function(e) {
-    this.data.penData.color = e.target.dataset.color;
-    this.setData({
-      showPenSet: false
-    })
-  },
-
-
-  openErraserSet: function () {
-    this.setData({
-      isClear: !this.data.isClear
-    })
-  },
-  
   clearAll: function () {
     var that = this;
     wx.showModal({
@@ -256,7 +204,7 @@ Page({
     var that = this;
     //上传点坐标信息
     wx.request({
-      url: 'http://118.89.161.135:8765/DrawSketch/finish',
+      url: 'https://78413126.draw3dsketch.com/finish',
       method: 'post',
       data: {
         //这里是发送给服务器的参数（参数名：参数值）
